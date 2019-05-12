92e8feb20fbb23f977479b2e3981ffc1affc7748
trabnett <timrabnett@gmail.com>
2019-05-12 08:01:26 -0400

    removed helper function. This is a functioning solution, but it wont submit into hackerrank.

--- code.py
+++ code.py
@@ -1,28 +1,22 @@
-def digits(num):
-    counter = 1
-    digits = 0
-    while counter <= num:
-        digits +=1
-        counter = counter * 10
-    return digits
-
-# print(digits(11),"function")
-
-x = 11100
+# def digits(num):
+#     counter = 1
+#     digits = 0
+#     while counter <= num:
+#         digits +=1
+#         counter = counter * 10
+#     return digits
+n = 101
 y = 1
 res = 0
-while y <= x:
-    dig = digits(y)
+while y <= n:
+    counter = 1
+    dig = 0
+    while counter <= y:
+        dig += 1
+        counter = counter * 10
     res = (res * (10 ** dig))
     res += y
     y += 1
 print(res)
 
-# x = 1000
-# counter = 1
-# digits = 0
-# while counter <= x:
-#     digits += 1
-#     counter = counter * 10
 
-# print(digits, counter)
